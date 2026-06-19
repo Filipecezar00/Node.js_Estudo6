4f5bb8531e3beb1d0b58665b1b3786344d1112df
Filipecezar00 <cezarfilipe01@gmail.com>
2026-06-19 10:04:56 -0300

    atualizando

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,34 +1,29 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
+import { Database } from "../../middlewares/database.js";
 
-const users = [];
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  const buffers = [];
-  for await (const chunck of req) {
-    buffers.push(chunck);
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch (error) {
-    req.body = null;
-  }
+  await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
     return res
       .setHeader("Content-type", "application/json")
       .end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    });
+    };
 
+    database.insert("users", user);
     return res.writeHead(201).end();
   }
 
